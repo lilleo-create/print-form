a06b5b91fc371b194d5e8e595e40890be7129726
Leonid Svetikov <laggerfint@yandex.ru>
2026-01-17 01:55:21 +0300

    Yandex.map

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,8 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './styles/global.css';
 
+console.log('YMAPS KEY:', import.meta.env.VITE_YMAPS_API_KEY);
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
