8a1d1943c99f49858808ecb676564c1484a9df64
Leonid Svetikov <laggerfint@yandex.ru>
2026-02-19 17:19:21 +0300

    Remove .env from repo

---
+++ backend/prisma/migrations/20260219134114_add_dropoff_schedule/migration.sql
@@ -0,0 +1,16 @@
+/*
+  Warnings:
+
+  - The values [WEEKDAYS] on the enum `DropoffSchedule` will be removed. If these variants are still used in the database, this will fail.
+
+*/
+-- AlterEnum
+BEGIN;
+CREATE TYPE "DropoffSchedule_new" AS ENUM ('DAILY', 'WEEKLY');
+ALTER TABLE "SellerDeliveryProfile" ALTER COLUMN "dropoffSchedule" DROP DEFAULT;
+ALTER TABLE "SellerDeliveryProfile" ALTER COLUMN "dropoffSchedule" TYPE "DropoffSchedule_new" USING ("dropoffSchedule"::text::"DropoffSchedule_new");
+ALTER TYPE "DropoffSchedule" RENAME TO "DropoffSchedule_old";
+ALTER TYPE "DropoffSchedule_new" RENAME TO "DropoffSchedule";
+DROP TYPE "DropoffSchedule_old";
+ALTER TABLE "SellerDeliveryProfile" ALTER COLUMN "dropoffSchedule" SET DEFAULT 'DAILY';
+COMMIT;
