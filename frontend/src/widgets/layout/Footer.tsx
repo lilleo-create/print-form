e92c66017548a3f5c69eade48c09949b1dfe06f6
leonid <LaggerFint@yandex.ru>
2026-03-20 16:04:41 +0300

    поменялось название и футер

--- frontend/src/widgets/layout/Footer.tsx
+++ frontend/src/widgets/layout/Footer.tsx
@@ -1,22 +1,48 @@
+import { Link } from 'react-router-dom';
 import styles from './Layout.module.css';
 
 export const Footer = () => {
   return (
     <footer className={styles.footer}>
       <div>
-        <h4>3D Печать маркетплейс</h4>
-        <p>Сервис изготовления, покупки и кастомной печати моделей.</p>
+        <h4>Print-Form</h4>
+        <p>
+          Маркетплейс 3D-печати: покупка, продажа и кастомное изготовление
+          моделей.
+        </p>
       </div>
+
       <div>
         <h5>Контакты</h5>
-        <p>support@3dmarket.ru</p>
-        <p>+7 (800) 555-15-15</p>
+        <a href="mailto:print.form@mail.ru">print.form@mail.ru</a>
       </div>
+
       <div>
-        <h5>Сервисы</h5>
-        <p>Каталог</p>
-        <p>Кастомная печать</p>
-        <p>Личный кабинет</p>
+        <h5>Покупателю</h5>
+        <Link to="/orders">Заказы</Link>
+        <Link to="/favorites">Избранные</Link>
+        <Link to="/account?tab=purchases">Покупки</Link>
+        <Link to="/account?tab=returns">Возвраты</Link>
+      </div>
+
+      <div>
+        <h5>Аккаунт</h5>
+        <Link to="/account?tab=profile">Профиль</Link>
+        <Link to="/account?tab=settings">Настройки</Link>
+        <Link to="/account?tab=chats">Чаты</Link>
+        <Link to="/seller">Кабинет продавца</Link>
+      </div>
+
+      <div>
+        <h5>Сервис</h5>
+        <Link to="/">Каталог</Link>
+        <Link to="/privacy-policy">О сервисе</Link>
+        <Link to="/shop">Ваш магазин</Link>
+      </div>
+
+      <div className={styles.footerBottom}>
+        <div>© 2026 Print-Form</div>
+        <div>Проект компании Print-Form</div>
       </div>
     </footer>
   );
